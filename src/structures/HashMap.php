9791cd45ccc129741387890026a6dd71690e01bd
Perf2k2 <perf2k2@gmail.com>
2018-09-10 20:36:18 +0300

    Method for incrementing hash map field

--- src/structures/HashMap.php
+++ src/structures/HashMap.php
@@ -34,6 +34,24 @@ class HashMap extends Structure
     /**
      * @param string $key
      * @param string $field
+     * @param float|int $number
+     * @return float
+     * @throws Exception
+     */
+    public function increment(string $key, string $field, $number): float
+    {
+        if (is_float($number)) {
+            return $this->conn->hIncrByFloat($this->getRedisKey($key), $field, $number);
+        } elseif (is_int($number)) {
+            return $this->conn->hIncrBy($this->getRedisKey($key), $field, $number);
+        } else {
+            throw new Exception("Number for increment field '{$field}' must be integer or float");
+        }
+    }
+
+    /**
+     * @param string $key
+     * @param string $field
      * @return string|bool
      */
     public function getValue(string $key, string $field)
